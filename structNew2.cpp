3c398b263accce74eef0976f9a9d1bfff123b2a7
Wulandary14 <Moonywulan14@gmail.com>
2026-04-06 18:40:38 +0700

    mengisi dan tampilkan

--- structNew2.cpp
+++ structNew2.cpp
@@ -13,3 +13,21 @@ struct Orang
     DetailAlamat;
     int umur;
 };
+
+int  main () {
+    Orang mhs;
+    //mengisi data
+    cout << "mengisi data" << endl;
+    cout << "nama : " ;
+    cin >> mhs.nama;
+    cout << "Alamat : " ;
+    cin >> mhs.alamat;
+    cout << "umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
